ccdfa9d14131bbf8551a50a4042d1c8f3110c286
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2022-12-11 02:23:16 -0500

    DOMContentLoaded

--- src/js/base.js
+++ src/js/base.js
@@ -4,35 +4,29 @@ document.head.innerHTML +=
 	"<style type='text/css'>:root {--theme-hue: " +
 	Math.random() * 360 +
 	"deg;}</style>";
+document.addEventListener("DOMContentLoaded", () => {
+	const headings = document.body.querySelectorAll("h1, h2, h3, h4, h5, h6");
+	for (let i = 0; i < headings.length; i++) {
+		headings[i].setAttribute("id", i);
+		headings[i].innerHTML =
+			"<a href='#" + i + "'>" + headings[i].innerHTML + "</a>";
+	}
 
-let headings = [].concat(
-	...document.body.getElementsByTagName("h1"),
-	...document.body.getElementsByTagName("h2"),
-	...document.body.getElementsByTagName("h3"),
-	...document.body.getElementsByTagName("h4"),
-	...document.body.getElementsByTagName("h5"),
-	...document.body.getElementsByTagName("h6")
-);
-for (let i = 0; i < headings.length; i++) {
-	headings[i].setAttribute("id", i);
-	headings[i].innerHTML =
-		"<a href='#" + i + "'>" + headings[i].innerHTML + "</a>";
-}
+	const pres = [...document.getElementsByTagName("pre")];
+	for (let i = 0; i < pres.length; i++) {
+		pres[i].innerHTML =
+			"<code class='language-" +
+			pres[0].getAttribute("lang") +
+			"'>" +
+			pres[i].innerHTML +
+			"</code>";
+	}
+	hljs.highlightAll();
 
-const pres = [...document.getElementsByTagName("pre")];
-for (let i = 0; i < pres.length; i++) {
-	pres[i].innerHTML =
-		"<code class='language-" +
-		pres[0].getAttribute("lang") +
-		"'>" +
-		pres[i].innerHTML +
-		"</code>";
-}
-hljs.highlightAll();
-
-const materialSymbols = document.body.querySelectorAll(
-	"[class|=material-symbols]"
-);
-for (let i = 0; i < materialSymbols.length; i++) {
-	materialSymbols[i].setAttribute("translate", "no");
-}
+	const materialSymbols = document.body.querySelectorAll(
+		"[class|=material-symbols]"
+	);
+	for (let i = 0; i < materialSymbols.length; i++) {
+		materialSymbols[i].setAttribute("translate", "no");
+	}
+});

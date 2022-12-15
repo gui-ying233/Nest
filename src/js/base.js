5e04924f07ab5e990162f913f594091d187cd4d6
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2022-12-15 13:54:01 -0500

    + battery, network status

--- src/js/base.js
+++ src/js/base.js
@@ -33,6 +33,131 @@ document.addEventListener("DOMContentLoaded", () => {
 		document.head.appendChild(preScript);
 	}
 
+	const connection =
+		navigator.connection ||
+		navigator.mozConnection ||
+		navigator.webkitConnection;
+	let connectionType;
+	function updateConnectionStatus() {
+		switch (connection.type) {
+			case "bluetooth":
+			case "wifi":
+				connectionType = connection.type;
+				break;
+			case "mixed":
+			case "other":
+			case "wimax":
+				connectionType = "sensors";
+				break;
+			case "cellular":
+				connectionType = "signal_cellular_alt";
+				break;
+			case "ethernet":
+				connectionType = "settings_ethernet";
+				break;
+			case "none":
+				connectionType = "wifi_off";
+			case "unknown":
+			default:
+				connectionType = "signal_wifi_statusbar_not_connected";
+				break;
+		}
+		document.body.querySelector(
+			"header > #network > .material-symbols-rounded"
+		).innerText = connectionType;
+		document.getElementById("network").title = connection.type;
+	}
+	connection.addEventListener("change", updateConnectionStatus);
+	updateConnectionStatus();
+
+	navigator.getBattery().then((battery) => {
+		switch (Math.round(battery.level) * 7) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				document.body.querySelector(
+					"header > #battery > .material-symbols-rounded"
+				).innerText =
+					"battery_" + Math.round(battery.level) * 7 + "_bar";
+				document.getElementById("battery").title =
+					Math.round(battery.level * 100) + "%";
+				break;
+			case 7:
+				document.body.querySelector(
+					"header > #battery > .material-symbols-rounded"
+				).innerText = "battery_full";
+				document.getElementById("battery").title =
+					Math.round(battery.level * 100) + "%";
+				break;
+			default:
+				document.body.querySelector(
+					"header > #battery > .material-symbols-rounded"
+				).innerText = "battery_unknown";
+		}
+		battery.onlevelchange = () => {
+			switch (Math.round(battery.level) * 7) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+					document.body.querySelector(
+						"header > #battery > .material-symbols-rounded"
+					).innerText =
+						"battery_" + Math.round(battery.level) * 7 + "_bar";
+					document.getElementById("battery").title =
+						Math.round(battery.level * 100) + "%";
+					break;
+				case 7:
+					document.body.querySelector(
+						"header > #battery > .material-symbols-rounded"
+					).innerText = "battery_full";
+					document.getElementById("battery").title =
+						Math.round(battery.level * 100) + "%";
+					break;
+				default:
+					document.body.querySelector(
+						"header > #battery > .material-symbols-rounded"
+					).innerText = "battery_unknown";
+			}
+		};
+		battery.onchargingchange = () => {
+			switch (Math.round(battery.level) * 7) {
+				case 0:
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+					document.body.querySelector(
+						"header > #battery > .material-symbols-rounded"
+					).innerText =
+						"battery_" + Math.round(battery.level) * 7 + "_barBolt";
+					document.getElementById("battery").title =
+						Math.round(battery.level * 100) + "%";
+					break;
+				case 7:
+					document.body.querySelector(
+						"header > #battery > .material-symbols-rounded"
+					).innerText = "battery_fullBolt";
+					document.getElementById("battery").title =
+						Math.round(battery.level * 100) + "%";
+					break;
+				default:
+					document.body.querySelector(
+						"header > #battery > .material-symbols-rounded"
+					).innerText = "battery_unknownBolt";
+			}
+		};
+	});
+
 	const materialSymbols = document.body.querySelectorAll(
 		"[class|=material-symbols]"
 	);

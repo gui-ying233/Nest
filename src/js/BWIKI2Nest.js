04d15b727c1c6dd91200c15fbec650731d4da1a1
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2026-03-04 03:49:16 -0500

    Update BWIKI2Nest.js

--- src/js/BWIKI2Nest.js
+++ src/js/BWIKI2Nest.js
@@ -1,2 +1,1 @@
-"use strict";
-window。location。href="https://gui-ying233.github.io/Nest/?utm_source=bwiki&utm_medium=profile&utm_content="+window.location.href;
+window.location.href="https://gui-ying233.github.io/Nest/?utm_source=bwiki&utm_medium=profile&utm_content="+window.location.href;
